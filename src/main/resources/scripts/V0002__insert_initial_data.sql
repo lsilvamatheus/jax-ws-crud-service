5ebe3d7b352a4dc170ed85ae998530ffbcabaa99
lsilvamatheus <lsilva.matheus@gmail.com>
2022-10-02 05:45:10 -0300

    Criando script inicial de insert de dados para teste

---
+++ src/main/resources/scripts/V0002__insert_initial_data.sql
@@ -0,0 +1,5 @@
+insert into noticia(
+	id, titulo, texto, data_publicacao)
+	values
+		(1, '1ª notícia', 'Esta é a primeira notícia cadastrada.', '2022-10-02 05:21:44'),
+		(2, '2ª notícia', 'Esta é a segunda notícia cadastrada.', '2022-10-02 05:22:00');
