f1b11dedab6285878a50303c4bd4972b9fd76219
lsilvamatheus <lsilva.matheus@gmail.com>
2022-10-02 06:00:42 -0300

    Alterando exception lançada nos métodos da interfacce

--- src/main/java/com/lsilva/matheus/jaxwscrudservice/repository/NoticiaRepository.java
+++ src/main/java/com/lsilva/matheus/jaxwscrudservice/repository/NoticiaRepository.java
@@ -1,14 +1,15 @@
 package com.lsilva.matheus.jaxwscrudservice.repository;
 
+import com.lsilva.matheus.jaxwscrudservice.config.databaseconfig.DbException;
 import com.lsilva.matheus.jaxwscrudservice.model.Noticia;
-import java.sql.SQLException;
+
 import java.util.List;
 
 public interface NoticiaRepository {
 
-    Long adicionar(Noticia noticia) throws SQLException;
-    Noticia alterar(Noticia noticia) throws SQLException;
-    boolean apagar(Long id) throws SQLException;
-    List<Noticia> listar() throws SQLException;
-    Noticia ler(Long id) throws SQLException;
+    Long adicionar(Noticia noticia) throws DbException;
+    Noticia alterar(Noticia noticia) throws DbException;
+    boolean apagar(Long id) throws DbException;
+    List<Noticia> listar() throws DbException;
+    Noticia ler(Long id) throws DbException;
 }
